820c34d037a6ed9c5068bdb7fc16a68b2f32d982
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-09-27 15:34:38 +1300

    added test

---
+++ v5.1/hello.go
@@ -0,0 +1,18 @@
+// Package main provides ...
+package  main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+  if name == "" {
+    name = "World"
+
+  }
+  return englishHelloPrefix + name
+}
+
+func main() {
+  fmt.Println(Hello("Chris"))
+}

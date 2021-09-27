d922a57339e6999a94dc562a5bcc4a6a20c16364
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-09-28 08:53:10 +1300

    Added switch case example

---
+++ v7/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix  = "Bonjour, "
+
+func Hello(name string, language string) string {
+
+  if name == "" {
+    name = "World"
+
+  }
+
+  prefix := englishHelloPrefix
+
+  switch language {
+  case spanish:
+    prefix = spanishHelloPrefix
+  case french:
+    prefix = frenchHelloPrefix
+  }
+
+  return prefix + name
+
+
+}
+
+func main() {
+  fmt.Println(Hello("World", ""))
+}
